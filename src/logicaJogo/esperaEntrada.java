39641e27b9ae248f21eacbf00b91ce89143ea6fc
unknown <a21230528@DEIS-L13C05.isec.pt>
2015-04-27 15:34:17 +0100

    Finalizados estados

---
+++ src/logicaJogo/esperaEntrada.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package logicaJogo;
+
+/**
+ *
+ * @author a21230528
+ */
+public class esperaEntrada implements Estado{
+
+    @Override
+    public void doAction() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
